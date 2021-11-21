c3356eb680cd86e8980ad1167b7f4e656cbcd8c8
Bratislav Petkovic <brp221@lehigh.edu>
2021-11-21 12:21:59 -0500

    bar chart update

--- App.R
+++ App.R
@@ -6,10 +6,12 @@ library(lubridate)
 library(remotes)
 #install_github("yonghah/esri2sf")
 library(esri2sf)
-setwd("C:/Users/p99br/Desktop/CSE281/EW_Shiny_App/")
+#setwd("C:/Users/p99br/Desktop/CSE281/EW_Shiny_App/")
+setwd("/Users/bratislavpetkovic/Desktop/CSE_COURSES/CSE281/EW_Shiny_App/")
+
 
 #energy data frame(s)
-energy_df <- read.csv("AnnualData.csv")
+#energy_df <- read.csv("AnnualData.csv")
 sample_energy_df <- read.csv("sampleEnergyData.csv")
 sample_energy_df$time <- ymd_hms(sample_energy_df$time)
 
@@ -25,12 +27,13 @@ energy_building_metadata <- merge(sample_energy_df, bldng_metadata, by="BUILDING
 shinyApp(
   ui = fluidPage(
     selectizeInput('BUILDINGID', 'Select BUILDINGID', choices = c("choose" = "", unique(sample_energy_df$BUILDINGID))),
-    sliderInput("slider", "Number of observations:", 1800, max(energy_building_metadata$YEARBUILT), 1950),
-    checkboxGroupInput("variable", "Variables to show:",
+    sliderInput("YearBuilt", "Number of observations:", 1800, max(energy_building_metadata$YEARBUILT), 1950),
+    checkboxGroupInput("BTYPES", "Building Types:",
                        unique(energy_building_metadata$BUILDINGTY)),
     mainPanel(
       plotOutput("ts_plot"),
       plotOutput("ts_plot_line"),
+      plotOutput("btype_avg_wh"),
       #tableOutput("table")
     )
   ),
@@ -39,6 +42,11 @@ shinyApp(
     filter_BID <- reactive({
       
       sample_energy_df %>% filter(BUILDINGID == input$BUILDINGID)
+    })
+    filter_BTYPE <- reactive({
+      
+      energy_building_metadata %>% filter(BUILDINGTY == input$BTYPES)
+      
     })
     
     # plot time series
@@ -57,6 +65,16 @@ shinyApp(
       #+ geom_area() 
       #+ scale_x_date(date_labels = "%b %Y")
       
+    })
+    output$btype_avg_wh <- renderPlot({
+      energy_building_metadata <- filter_BTYPE()
+      avg_watt_btype <- energy_building_metadata %>% group_by(BUILDINGTY)%>% summarise(WattHours = mean(WattHours))
+      
+      # 3: Using RColorBrewer
+      ggplot(energy_building_metadata, aes(x=BUILDINGTY, y = WattHours,  fill=BUILDINGTY )) + 
+        geom_bar(stat = "identity") +
+        scale_fill_brewer(palette = "Set1") +
+        theme(legend.position="none")
     })
     
     output$table <- renderTable({
--- TrialCode.R
+++ TrialCode.R
@@ -1,9 +1,19 @@
 library(remotes)
 #install_github("yonghah/esri2sf")
 library(esri2sf)
+library(ggplot2)
 
-bldng_metadata_all <- esri2sf(
-  "https://services.arcgis.com/VXxCfMpXUKuFKFvE/arcgis/rest/services/Building/FeatureServer/0",
-)
+# 3: Using RColorBrewer
+ggplot(mtcars, aes(x=as.factor(cyl), fill=as.factor(cyl) )) + 
+  geom_bar( ) +
+  scale_fill_brewer(palette = "Set1") +
+  theme(legend.position="none")
 
-bldng_metadata <- bldng_metadata_all[, c("BUILDINGID", "BUILDINGTY", "GROSSAREA","NETAREA", "YEARBUILT",  "Shape__Area")]
+
+avg_watt_btype <- energy_building_metadata %>% group_by(BUILDINGTY)%>% summarise(WattHours = mean(WattHours))
+
+# 3: Using RColorBrewer
+ggplot(energy_building_metadata, aes(x=BUILDINGTY, y = WattHours,  fill=BUILDINGTY )) + 
+  geom_bar(stat = "identity") +
+  scale_fill_brewer(palette = "Set1") +
+  theme(legend.position="none")
