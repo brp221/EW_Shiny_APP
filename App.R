3f6a2d2afbd61dc2d2c1d158974fa2e7df73d62f
Bratislav Petkovic <brp221@lehigh.edu>
2021-11-26 11:51:05 -0500

    Make a dashbaord yay

--- App.R
+++ App.R
@@ -6,6 +6,7 @@ library(lubridate)
 library(remotes)
 install_github("yonghah/esri2sf", force = 'TRUE')
 library(esri2sf)
+library(shinydashboard)
 #setwd("C:/Users/p99br/Desktop/CSE281/EW_Shiny_App/")
 setwd("/Users/bratislavpetkovic/Desktop/CSE_COURSES/CSE281/EW_Shiny_App/")
 
@@ -24,97 +25,122 @@ bldng_metadata <- bldng_metadata_all[, c("BUILDINGID", "BUILDINGTY", "GROSSAREA"
 energy_building_metadata <- merge(sample_energy_df, bldng_metadata, by="BUILDINGID" )
 
 # demoing group support in the `choices` arg
-shinyApp(
-  ui = fluidPage(
-    sidebarLayout(
-      sidebarPanel(
-        #radio to select by buildingID
-        selectizeInput('BUILDINGID', 'Select BUILDINGID', choices = c("choose" = "", unique(sample_energy_df$BUILDINGID))),
-      ), 
-      mainPanel(
-        plotOutput("ts_plot"),
-        plotOutput("ts_plot_line"),
-      )
-    ),
-    #radio to select by buildingID
-    #selectizeInput('BUILDINGID', 'Select BUILDINGID', choices = c("choose" = "", unique(sample_energy_df$BUILDINGID))),
-    #slider to select buildings by yearbuilt 
-    sliderInput("YEARBUILT", "Year Built:", 1800, max(energy_building_metadata$YEARBUILT), value = c(1875,1940)),
-    #radio group to select buildings by their building type
-    checkboxGroupInput("BTYPES", "Building Types:", unique(energy_building_metadata$BUILDINGTY), selected =unique(energy_building_metadata$BUILDINGTY)),
-    #radio to select by BUILDINGTY
-    selectizeInput('BUILDINGTY', 'Select BUILDINGTY', choices = c("choose" = "", unique(energy_building_metadata$BUILDINGTY))),
-    
-    mainPanel(
-      plotOutput("btype_avg_wh"),
-      plotOutput("scatter_plot_year"),
-      plotOutput("boxplot_btype"),
-      #tableOutput("table")
-    )
+ui <- dashboardPage(
+  dashboardHeader(title = "Lehigh Energy Dashboard"),
+  dashboardSidebar(
+    menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+    menuItem("lightbulb", tabName = "lightbulb", icon = icon("lightbulb")),
+    menuItem("clock", tabName = "clock", icon = icon("clock")),
+    menuItem("plug", tabName = "plug", icon = icon("plug"))
   ),
-  server = function(input, output) {
-    
-    filter_BID <- reactive({
-      energy_building_metadata %>% filter(BUILDINGID == input$BUILDINGID)
-    })
-    filter_BTYPE <- reactive({
-      energy_building_metadata %>% filter(BUILDINGTY == input$BTYPES)
-    })
-    filter_BTYPE_group <- reactive({
-      energy_building_metadata %>% filter(BUILDINGTY == input$BUILDINGTY)
-    })
-    filter_year_built <- reactive({
-      energy_building_metadata %>% filter(YEARBUILT > input$YEARBUILT[1], YEARBUILT < input$YEARBUILT[2])
-    })
-    
-    # plot time series
-    output$ts_plot <- renderPlot({
+  dashboardBody(
+    tabItems(
+      # First tab content
+      tabItem(tabName = "dashboard",
+              fluidRow(
+                box(plotOutput("ts_plot", height = 250)),
+                
+                box(
+                  title= "Controls",
+                  #radio to select by buildingID
+                  selectizeInput('BUILDINGID', 'Select BUILDINGID', choices = c("choose" = "", unique(sample_energy_df$BUILDINGID))),
+                )
+              )
+      ),
       
-      energy_building_metadata <- filter_BID()
-      ggplot(energy_building_metadata, aes(x = time, y=WattHours)) + geom_area()
-       
-      #+ scale_x_date(date_labels = "%b %Y")
-      
-    })
-    output$ts_plot_line <- renderPlot({
-      
-      energy_building_metadata <- filter_BID()
-      ggplot(energy_building_metadata, aes(x = time, y=WattHours)) + geom_line()
-      #+ geom_area() 
-      #+ scale_x_date(date_labels = "%b %Y")
-      
-    })
-    output$range <- renderPrint({ input$YEARBUILT })
-    
-    # avg watts by building type bar chart 
-    output$btype_avg_wh <- renderPlot({
-      energy_building_metadata <- filter_BTYPE()
-      avg_watt_btype <- energy_building_metadata %>% group_by(BUILDINGTY)%>% summarise(WattHours = mean(WattHours))
-      
-      # 3: Using RColorBrewer
-      ggplot(energy_building_metadata, aes(x=BUILDINGTY, y = WattHours,  fill=BUILDINGTY )) + 
-        geom_bar(stat = "identity") +
-        scale_fill_brewer(palette = "Set1") +
-        theme(legend.position="none")
-    })
-    
-    output$scatter_plot_year <- renderPlot({
-      energy_building_metadata <- filter_year_built()
-      sum_watts_date_range <- energy_building_metadata %>% group_by(BUILDINGID)%>% summarise(WattHours = mean(WattHours))
-      # A basic scatterplot with color depending on Species
-      ggplot(energy_building_metadata, aes(x=YEARBUILT, y=WattHours, color=BUILDINGTY)) + 
-        geom_point(size=2) 
-    })
-    
-    output$boxplot_btype <- renderPlot({
-      energy_building_metadata <- filter_BTYPE_group()
-      ggplot(energy_building_metadata, aes(x=BUILDINGTY, y=WattHours)) + 
-      geom_boxplot() + geom_jitter(shape=16, position=position_jitter(0.2))
-      
-    })
-    
-    output$table <- renderTable({
-      filter_BID()
-    })
-  }
+      # Second tab content
+      tabItem(tabName = "lightbulb",
+              fluidRow(
+                box(
+                  plotOutput("scatter_plot_year", height = 250),
+                ),
+                box(
+                  title= "Controls",
+                  #slider to select buildings by yearbuilt 
+                  sliderInput("YEARBUILT", "Year Built:", 1800, max(energy_building_metadata$YEARBUILT), value = c(1800,2017)),
+                )
+              )
+      ),
+      # Third tab content
+      tabItem(tabName = "clock",
+              fluidRow(
+                box(plotOutput("btype_avg_wh", height = 250),
+                ),
+                box(
+                  title= "Controls",
+                  #radio group to select buildings by their building type
+                  checkboxGroupInput("BTYPES", "Building Types:", unique(energy_building_metadata$BUILDINGTY), selected =unique(energy_building_metadata$BUILDINGTY)),
+                )
+              )
+      ),
+      # Fourth tab content
+      tabItem(tabName = "plug",
+              fluidRow(
+                box(
+                  plotOutput("boxplot_btype", height = 250),
+                ),
+                box(
+                  title= "Controls",
+                  #radio to select by BUILDINGTY
+                  selectizeInput('BUILDINGTY', 'Select BUILDINGTY', choices = c("choose" = "", unique(energy_building_metadata$BUILDINGTY))),
+                  actionButton("Filter_0", "Filter 0 Values"),
+                  )
+              )
+      )
+    )
+  )
 )
+server <- function(input, output) {
+  
+  filter_BID <- reactive({
+    energy_building_metadata %>% filter(BUILDINGID == input$BUILDINGID)
+  })
+  filter_year_built <- reactive({
+    energy_building_metadata %>% filter(YEARBUILT > input$YEARBUILT[1], YEARBUILT < input$YEARBUILT[2])
+  })
+  filter_BTYPE <- reactive({
+    energy_building_metadata %>% filter(BUILDINGTY == input$BTYPES)
+  })
+  filter_BTYPE_group <- reactive({
+    energy_building_metadata %>% filter(BUILDINGTY == input$BUILDINGTY)
+  })
+  filter_0_vals <- eventReactive(input$go, {
+    energy_building_metadata %>% filter(WattHours != 0)
+    #runif(input$BUILDINGTY)
+  })
+  # plot time series
+  output$ts_plot <- renderPlot({
+    energy_building_metadata <- filter_BID()
+    ggplot(energy_building_metadata, aes(x = time, y=WattHours)) + geom_area()
+    #+ scale_x_date(date_labels = "%b %Y")
+  })
+  output$scatter_plot_year <- renderPlot({
+    energy_building_metadata <- filter_year_built()
+    sum_watts_date_range <- energy_building_metadata %>% group_by(BUILDINGID)%>% summarise(WattHours = mean(WattHours))
+    # A basic scatterplot with color depending on Species
+    ggplot(energy_building_metadata, aes(x=YEARBUILT, y=WattHours, color=BUILDINGTY)) + 
+      geom_point(size=2) 
+  })
+  # avg watts by building type bar chart 
+  output$btype_avg_wh <- renderPlot({
+    energy_building_metadata <- filter_BTYPE()
+    avg_watt_btype <- energy_building_metadata %>% group_by(BUILDINGTY)%>% summarise(WattHours = mean(WattHours))
+    
+    # 3: Using RColorBrewer
+    ggplot(energy_building_metadata, aes(x=BUILDINGTY, y = WattHours,  fill=BUILDINGTY )) + 
+      geom_bar(stat = "identity") +
+      scale_fill_brewer(palette = "Set1") +
+      theme(legend.position="none")
+  })
+  
+  output$boxplot_btype <- renderPlot({
+    energy_building_metadata <- filter_BTYPE_group()
+    energy_building_metadata <- filter_0_vals()
+    ggplot(energy_building_metadata, aes(x=BUILDINGTY, y=WattHours)) + 
+      geom_boxplot() + geom_jitter(shape=16, position=position_jitter(0.2))
+    
+  })
+  
+}
+shinyApp(ui,server)
+
--- TrialCode.R
+++ TrialCode.R
@@ -1,75 +1,89 @@
-library(remotes)
-#install.packages("remotes") when esri2sf not working 
-#install_github("yonghah/esri2sf")
-library(esri2sf)
+library(shiny)
+library(magrittr)
+library(dplyr)
 library(ggplot2)
-
-avg_watt_btype <- energy_building_metadata %>% group_by(BUILDINGTY)%>% summarise(WattHours = mean(WattHours))
-
-
-#energy_building_metadata <- filter_year_built()
-sum_watts_date_range <- energy_building_metadata %>% group_by(BUILDINGID)%>% summarise(WattHours = mean(WattHours))
-# A basic scatterplot with color depending on Species
-ggplot(sum_watts_date_range, aes(x=YEARBUILT, y=WattHours)) + 
-  geom_point(size=6) 
+library(lubridate)
+library(remotes)
+install_github("yonghah/esri2sf", force = 'TRUE')
+library(esri2sf)
+library(shinydashboard)
+#setwd("C:/Users/p99br/Desktop/CSE281/EW_Shiny_App/")
+setwd("/Users/bratislavpetkovic/Desktop/CSE_COURSES/CSE281/EW_Shiny_App/")
 
 
+#energy data frame(s)
+#energy_df <- read.csv("AnnualData.csv")
+sample_energy_df <- read.csv("sampleEnergyData.csv")
+sample_energy_df$time <- ymd_hms(sample_energy_df$time)
 
-energy_building_metadata <- energy_building_metadata %>% filter(YEARBUILT < 1950, YEARBUILT > 1800)
+#building metadata
+bldng_metadata_all <- esri2sf(
+  "https://services.arcgis.com/VXxCfMpXUKuFKFvE/arcgis/rest/services/Building/FeatureServer/0",
+)
+bldng_metadata <- bldng_metadata_all[, c("BUILDINGID", "BUILDINGTY", "GROSSAREA","NETAREA", "YEARBUILT",  "Shape__Area")]
 
-energy_building_metadata <- filter_year_built()
-#sum_watts_date_range <- energy_building_metadata %>% group_by(BUILDINGID)%>% summarise(WattHours = mean(WattHours))
-# A basic scatterplot with color depending on Species
-ggplot(energy_building_metadata, aes(x=YEARBUILT, y=WattHours)) + 
-  geom_point(size=6) 
+energy_building_metadata <- merge(sample_energy_df, bldng_metadata, by="BUILDINGID" )
 
-
-# Notched Boxplot of Tooth Growth Against 2 Crossed Factors
-# boxes colored for ease of interpretation
-boxplot(len~supp*dose, data=ToothGrowth, notch=TRUE,
-        col=(c("gold","darkgreen")),
-        main="Tooth Growth", xlab="Suppliment and Dose")
-
-# Convert the variable dose from a numeric to a factor variable
-ToothGrowth$dose <- as.factor(ToothGrowth$dose)
-head(ToothGrowth)
-# Box plot with dot plot
-# Basic box plot
-p <- ggplot(ToothGrowth, aes(x=dose, y=len)) + 
-  geom_boxplot()
-p
-# Rotate the box plot
-p + coord_flip()
-# Notched box plot
-ggplot(ToothGrowth, aes(x=dose, y=len)) + 
-  geom_boxplot(notch=TRUE)
-# Change outlier, color, shape and size
-ggplot(ToothGrowth, aes(x=dose, y=len)) + 
-  geom_boxplot(outlier.colour="red", outlier.shape=8,
-               outlier.size=4)
-p + geom_dotplot(binaxis='y', stackdir='center', dotsize=1)
-# Box plot with jittered points
-# 0.2 : degree of jitter in x direction
-p + geom_jitter(shape=16, position=position_jitter(0.2))
-
-
-energy_building_metadata$BUILDINGTY = as.factor(energy_building_metadata$BUILDINGTY)
-q <- ggplot(energy_building_metadata, aes(x=BUILDINGTY, y=WattHours)) + 
-  geom_boxplot() + geom_jitter(shape=16, position=position_jitter(0.2))
-
-q
-# Rotate the box plot
-q + coord_flip()
-# Notched box plot
-q <- ggplot(energy_building_metadata, aes(x=BUILDINGTY, y=WattHours)) + 
-  geom_boxplot(notch=TRUE)
-# Change outlier, color, shape and size
-ggplot(energy_building_metadata, aes(x=BUILDINGTY, y=WattHours)) + 
-  geom_boxplot(outlier.colour="red", outlier.shape=8,
-               outlier.size=4)
-q + geom_dotplot(binaxis='y', stackdir='center', dotsize=1)
-# Box plot with jittered points
-# 0.2 : degree of jitter in x direction
-q + geom_jitter(shape=16, position=position_jitter(0.2))
-
+  
+ui <- dashboardPage(
+  dashboardHeader(title = "Basic dashboard"),
+  dashboardSidebar(
+    menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+    menuItem("lightbulb", tabName = "lightbulb", icon = icon("lightbulb"))
+  ),
+  dashboardBody(
+    tabItems(
+      # First tab content
+      tabItem(tabName = "dashboard",
+              fluidRow(
+                box(plotOutput("ts_plot", height = 250)),
+                
+                box(
+                  title= "Controls",
+                  #radio to select by buildingID
+                  selectizeInput('BUILDINGID', 'Select BUILDINGID', choices = c("choose" = "", unique(sample_energy_df$BUILDINGID))),
+                )
+              )
+      ),
+      
+      # Second tab content
+      tabItem(tabName = "lightbulb",
+              fluidRow(
+                box(
+                  plotOutput("scatter_plot_year", height = 250),
+                ),
+                box(
+                  title= "Controls",
+                  #slider to select buildings by yearbuilt 
+                  sliderInput("YEARBUILT", "Year Built:", 1800, max(energy_building_metadata$YEARBUILT), value = c(1800,2017)),
+                )
+              )
+      )
+    )
+  )
+)
+server <- function(input, output) {
+  
+  filter_BID <- reactive({
+    energy_building_metadata %>% filter(BUILDINGID == input$BUILDINGID)
+  })
+  filter_year_built <- reactive({
+    energy_building_metadata %>% filter(YEARBUILT > input$YEARBUILT[1], YEARBUILT < input$YEARBUILT[2])
+  })
+  # plot time series
+  output$ts_plot <- renderPlot({
+    energy_building_metadata <- filter_BID()
+    ggplot(energy_building_metadata, aes(x = time, y=WattHours)) + geom_area()
+    #+ scale_x_date(date_labels = "%b %Y")
+  })
+  output$scatter_plot_year <- renderPlot({
+    energy_building_metadata <- filter_year_built()
+    sum_watts_date_range <- energy_building_metadata %>% group_by(BUILDINGID)%>% summarise(WattHours = mean(WattHours))
+    # A basic scatterplot with color depending on Species
+    ggplot(energy_building_metadata, aes(x=YEARBUILT, y=WattHours, color=BUILDINGTY)) + 
+      geom_point(size=2) 
+  })
+    
+}
+shinyApp(ui,server)
 
